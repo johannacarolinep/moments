b29f1a52cbcbd453f640549c2c05e2209c6e6a8f
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-06-08 17:11:14 +0200

    fix: update base URL

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = 'https://drf-moments-ci-acc06eb4f190.herokuapp.com/';
-axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+axios.defaults.baseURL = 'https://drf-moments-ci-acc06eb4f190.herokuapp.com/';
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
