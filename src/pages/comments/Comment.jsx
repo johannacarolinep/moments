9acf9b389c8f23f8d4668f78e61f622fb9610795
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-06-09 14:29:28 +0200

    feat: Add dropdown to comments and function to handle deletion

--- src/pages/comments/Comment.jsx
+++ src/pages/comments/Comment.jsx
@@ -3,6 +3,9 @@ import styles from "../../styles/Comment.module.css";
 import { Media } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Avatar from '../../components/Avatar';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { MoreDropdown } from '../../components/MoreDropdown';
+import { axiosRes } from '../../api/axiosDefaults';
 
 const Comment = (props) => {
     const {
@@ -11,7 +14,32 @@ const Comment = (props) => {
         owner,
         updated_at,
         content,
+        id,
+        setPost,
+        setComments,
     } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/comments/${id}/`)
+            setPost(prevPost => ({
+                results: [{
+                    ...prevPost.results[0],
+                    comments_count: prevPost.results[0].comments_count - 1
+                },],
+            }));
+
+            setComments(prevComments => ({
+                ...prevComments,
+                results: prevComments.results.filter((comment) => comment.id !== id),
+            }));
+        } catch(err) {
+            console.log(err);
+        };
+    }
 
   return (
     <div>
@@ -29,6 +57,9 @@ const Comment = (props) => {
                 </span>
                 <p>{content}</p>
             </Media.Body>
+            {is_owner && (
+                <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete}/>
+            )}
         </Media>
     </div>
   )
